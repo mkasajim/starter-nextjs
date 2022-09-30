6c5b736ff11c1cb16442d222c956f89e133961f2
mkasajim <63280801+mkasajim@users.noreply.github.com>
2022-09-30 14:20:29 +0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,40 @@
-import '../styles/globals.css'
+import React from 'react';
+import {Typography, Table,TableRow, TableCell, tableCellClasses, AppBar, CssBaseline , Toolbar, Container} from '@mui/material'
+import BasicTabs from './Tab';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const receipt = require('./receipt')
+
+
+const App = () => {
+
+  return (
+    <>
+      <CssBaseline/>
+      <AppBar position='relative' elevation={0}>
+      <Toolbar>
+          <Table
+        sx={{
+          [`& .${tableCellClasses.root}`]: {
+            borderBottom: "none"
+          }
+        }}
+      >
+              <TableRow>
+                  <TableCell align='center'>
+                      <Typography variant='h4'>
+                      Smess
+                      </Typography>
+                  
+                  </TableCell>
+              </TableRow>
+          </Table>
+      </Toolbar>
+      </AppBar>
+      <BasicTabs/>
+
+    </>
+  );
 }
 
-export default MyApp
+
+export default App;
